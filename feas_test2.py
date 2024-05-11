a447035697097e11c22be2832ec90a55d1e3fbe3
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-05-12 00:02:06 +0100

    new edit

--- feas_test2.py
+++ feas_test2.py
@@ -9,50 +9,14 @@ import xlrd
 import streamlit as st
 import pandas as pd
 import pygsheets
+import shutil
 
-source = "/Users/suryaganesan/downloads/test_book.xlsx"
+# Source file
+source = "/Users/suryaganesan/vscode/ml/projects/reporter/sales_data_copy.xlsx"
 
-# Function to upload Excel file to Google Sheets
-def upload_to_google_sheets(uploaded_file):
-    # Authorization
-    gc = pygsheets.authorize(service_file='sheets_account.json')
+# Destination directory
+move_location = "/Users/suryaganesan/library/containers/com.microsoft.excel/data/"
 
-    # Open the Google Spreadsheet
-    sh = gc.create("uploaded_file.xlsx")
-    wks = sh[0]
-    # Upload Excel file to Google Sheets
-    df = pd.read_excel(uploaded_file)
-    wks.set_dataframe(df, (1,1))
+# Move the file
+shutil.copy(source, move_location)
 
-    return sh
-
-# Function to perform operations on the Google Sheet
-def perform_operations(sh):
-    # Access the first sheet
-    wks = sh[0]
-
-    # Read data from Google Sheet
-    df = wks.get_as_df()
-
-    # Perform operations
-    st.write("Data from Google Sheet:")
-    st.write(df)
-
-# Main function
-def main():
-    st.title("Upload Excel File to Google Sheets")
-
-    # Upload Excel file
-    uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
-
-    # Check if file is uploaded
-    if uploaded_file is not None:
-        # Upload Excel file to Google Sheets
-        sh = upload_to_google_sheets(uploaded_file)
-        print(sh.url)
-        print("Upload successful...")
-        # Perform operations on Google Sheet
-        perform_operations(sh)
-
-if __name__ == "__main__":
-    main()
