6fb328b8f9d81fb667c46cc2e0d3b7c58bcc4e18
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-05-16 12:16:01 +0100

    general edits

--- st_app.py
+++ st_app.py
@@ -16,15 +16,6 @@ from PoC_v3 import (
 )
 
 
-if 'key' not in st.session_state:
-    st.session_state.key = 'value'
-
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{
-        "role": "ai", 
-        "content": ""
-        }]
-
 def copy_excel_locally(file):
     fname, ext = os.path.splitext(file.name)
     new_fname = f"{fname}_copy{ext}"
@@ -73,11 +64,12 @@ def load_sheets_to_dfs(file_path):
     wb = app.books.open(file_path)
     dfs = []
     for sheet in wb.sheets:
-        df = sheet.used_range.options(pd.DataFrame, header=True, index=True).value
+        df = sheet.used_range.options(pd.DataFrame, header=True, index=False).value
         df.columns = handle_duplicate_columns(df.columns)
         dfs.append(df)
 
     sheet_names = [sheet.name for sheet in wb.sheets]
+    wb.save()
     wb.close()
     app.quit()
     return dfs, sheet_names
@@ -97,33 +89,59 @@ header_ph = st.empty()
 header_ph.markdown( "<h3 style='text-align: center;'>Command AI: Spend less time preparing your data for analysis</h3>", unsafe_allow_html=True)
 uploader_ph = st.empty()
 
-with uploader_ph.container():
-       uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
+# Two errors to fix:
+# 1. If I delete uploaded file, it still retains it to work on
+# 2. Not saving after change
 
-while uploaded_file is None:
-    continue
+select_sheet_key = "select-sheet"
+
+if 'key' not in st.session_state:
+    st.session_state.key = 'value'
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{
+        "role": "ai", 
+        "content": ""
+    }]
+
+if "file_path" not in st.session_state:
+   st.session_state.file_path = None
+
+if "cache_clear" not in st.session_state:
+    st.session_state.cache_clear = False
+
+
+def sheet_choice_01(sheets_list):
+    return st.sidebar.selectbox(
+        "Select sheet",
+        sheets_list
+    )
+
 
 def main():
-    if "file_path" not in st.session_state:
-        file_path = copy_excel_locally(uploaded_file)
-        st.session_state.file_path = file_path
-    else:
-        file_path = st.session_state.file_path
+    global cache_clear
 
-    if file_path is not None:
+    with uploader_ph.container():
+       uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
+
+    if uploaded_file is None:
+        st.session_state.file_path = None
+
+    if uploaded_file is not None:
+        if st.session_state.file_path is None:
+            file_path = copy_excel_locally(uploaded_file)
+            st.session_state.file_path = file_path
+
+#      if st.session_state.file_path is not None:
         # Save excel file
         st.markdown("<h4>Current State: </h4>", unsafe_allow_html=True)
         st.sidebar.title("Excel sheets")
         
-        dfs, sheets = load_sheets_to_dfs(file_path)
+        dfs, sheets = load_sheets_to_dfs(st.session_state.file_path)
 
-        select_sheet_ph = st.empty()
+        select_sheet_ph = st.sidebar.empty()
 
-        with select_sheet_ph.container():
-            current_sheet = st.sidebar.selectbox(
-                "Select sheet",
-                (sheets),
-            )
+        current_sheet = select_sheet_ph.radio("Select sheets", sheets)
 
         # Display current state
         current_table_placeholder = st.empty()
@@ -140,83 +158,94 @@ def main():
 
         if st.session_state.messages[-1]["role"] != "ai":
             if request:
-               with st.chat_message("ai"):
-                 if request != "quit":
-                    test = save_sheets(file_path)
-                    st.write(f"Saving and closing sheets: {test}")
+                with st.chat_message("ai"):
+                  if request != "quit":
+                    test = save_sheets(st.session_state.file_path)
+                    st.write(f"Saving and closing sheet: {test}")
                     st.write("Creating a plan...")
-                    plan = create_plan(request, file_path)
+                    print("Creating a plan...\n")
+                    plan = create_plan(request, st.session_state.file_path)
 
                     st.write(f"The plan: \n{plan}")
+                    print("The plan: \n", plan)
 
                     st.write("Generating script")
-                    script_generated = generate_code(request, file_path, plan)
+                    script_generated = generate_code(request, st.session_state.file_path, plan)
 
                     st.code(f"Code generated: \n {script_generated}")
+                    print("The code: \n", script_generated)
                     print("\n\nReviewing code...")
 
-                    reviewed_script = review_code(request, script_generated, file_path, plan)
+                    reviewed_script = review_code(request, script_generated, st.session_state.file_path, plan)
 
                     final_script = reviewed_script
 
                     st.code(f"#Script reviewed: \n{final_script}")
+                    print("Script reviewed: \n", final_script)
 
                     try:
                         st.write("\n\nInitiating code execution...")
                         exec(final_script)
                         status = save_sheets(st.session_state.file_path)
                         st.write(f"Saving changes: {status}")
+                        message = {"role": "ai", "content": f"{final_script}"}
+                        st.session_state.messages.append(message)
+                        cache_clear = True
 
                     except Exception as e:
                         st.write(f"Error: {e}")
                         st.write("Analysing the error....")
-                        new_code = refresh_code(request, e, file_path, plan, final_script)
+                        print("Got error: ", e)
+
+                        new_code = refresh_code(request, e, st.session_state.file_path, plan, final_script)
                         st.code(f"New code: \n {new_code}")
+                        print("New code: ", new_code)
 
                         try:
                             st.write("Initializing new refreshed code....")
                             exec(new_code)
                             status = save_sheets(st.session_state.file_path)
                             st.write(f"Saving changes: {status}")
+                            message = {"role": "ai", "content": f"{final_script}"}
+                            st.session_state.messages.append(message)
+                            cache_clear = True
 
                         except Exception as e2:
                             st.write(f"New Error: {e2}")
+                            print("Got a new error: ", e2)
+                    
+                    cache_clear = True
 
-#        refresh = st.button("Refresh")
-#        if refresh:
-                        st.cache_data.clear()  
+    if cache_clear:
+                  st.sidebar.empty()
+                  st.cache_data.clear()
+                  current_sheet = None
 
-                        dfs, sheets = load_sheets_to_dfs(file_path)
+                  with st.chat_message("ai"):
+                    st.write("Cache data should be cleared")
 
-                        print(dfs[0])
-                        print(file_path)
+                  dfs, sheets = load_sheets_to_dfs(st.session_state.file_path)
 
-                        current_table_placeholder.empty()
-                        select_sheet_ph.empty()
+                  print(dfs[0])
+                  print(st.session_state.file_path)
 
-                        with select_sheet_ph.container():
-                            current_sheet = st.sidebar.selectbox(
-                                "Select sheets",
-                                (sheets),
-                            )
+                  current_table_placeholder.empty()
+                  select_sheet_ph.empty()
 
-                        with current_table_placeholder.container():
-                            st.dataframe(dfs[sheets.index(current_sheet)])
+
+                  current_sheet = st.empty()
+                
+                  current_sheet = select_sheet_ph.radio("Select sheet",sheets)
+
+                  with current_table_placeholder.container():
+                      st.dataframe(dfs[sheets.index(current_sheet)])
+
+                  cache_clear = False
+
+                  
 
 if __name__ == "__main__":
+    cache_clear = False
     main()
 
-
-        # Display current state
-        
-
-        
-
-        
-
-
-
-
-
-    # Create buttons for each sheet
-    # if st.button show df for each sheet
+# If encountered error, use saved intermediate steps to continue
